e8c6d58b6ca28e23922b2b5e41f336272c0b06e2
nwshane <django09@gmail.com>
2015-04-16 10:48:32 +0400

    Fixed web urls for staging and production.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :domain, 'alpha.jumpstart.ge'
 set :user, 'prisoners-staging'
 set :application, 'Starter-Template-Production'
 set :repository, "git@github.com:JumpStartGeorgia/Starter-Template.git"
-set :web_url, "starter-prod.jumpstart.ge"
+set :web_url, "rails4-deploy.jumpstart.ge"
