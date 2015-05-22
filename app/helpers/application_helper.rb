bedf136a9740afd6d426d96b6fbb9b98c308bb60
nwshane <django09@gmail.com>
2015-05-22 17:55:43 +0400

    Added page header helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title(page_title)
+    content_for(:page_title) { page_title.html_safe }
+  end
+
+  def page_subtitle(page_subtitle)
+    content_for(:page_subtitle) { page_subtitle.html_safe }
+  end
 end
