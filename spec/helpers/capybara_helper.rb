1ced5dee64f03d61a3aa9ca966ab016c6b13c476
nwshane <django09@gmail.com>
2015-10-16 14:18:12 +0400

    Copied some rspec config from prisoners project, but commented it out for the time being because not sure if it's necessary.

--- spec/helpers/capybara_helper.rb
+++ spec/helpers/capybara_helper.rb
@@ -10,4 +10,26 @@ RSpec.configure do |config|
   end
 
   Capybara.current_driver = :selenium_chrome
+
+  #### TODO COMMENTED OUT BELOW CODE BECAUSE IT'S COPIED FROM PRISONERS PROJECT AND NOT SURE IF IT'S NEEDED
+
+  # # Helper function for Capybara to select options in multiple jQuery select2
+  # def select2_select_multiple(select_these, select_id)
+  #   # This methods requires @javascript in the Scenario
+  #   [select_these].flatten.each do |value|
+  #     clickable_input = find(:xpath, "//*[contains(@id, '#{select_id}')]//input[contains(@class, 'select2-input')]")
+  #     clickable_input.click
+  #     found = false
+  #     within('.select2-drop') do
+  #       all('li.select2-result').each do |result|
+  #         unless found
+  #           if result.text == value
+  #             result.click
+  #             found = true
+  #           end
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
 end
--- spec/helpers/locale_helper.rb
+++ spec/helpers/locale_helper.rb
@@ -6,4 +6,34 @@ RSpec.configure do |_config|
   def l(string, options = {})
     I18n.l(string, options)
   end
+
+  #### TODO COMMENTED OUT BELOW CODE BECAUSE IT'S COPIED FROM PRISONERS PROJECT AND NOT SURE IF IT'S NEEDED
+
+
+  # # Makes controllers work with default locale
+  # # Taken from http://stackoverflow.com/a/19079076/3115911
+  # class ActionController::TestCase
+  #   module Behavior
+  #     def process_with_default_locale(action, http_method = 'GET', parameters = nil, session = nil, flash = nil)
+  #       parameters = { locale: I18n.locale }.merge(parameters || {}) unless I18n.locale.nil?
+  #       process_without_default_locale(action, http_method, parameters, session, flash)
+  #     end
+  #     alias_method_chain :process, :default_locale
+  #   end
+  # end
+  #
+  # config.before(:each, type: :feature) do
+  #   default_url_options[:locale] = I18n.default_locale
+  # end
+  #
+  # config.before(:each, type: :request) do
+  #   default_url_options[:locale] = I18n.default_locale
+  # end
+  #
+  # # Makes routing specs work with default locale
+  # class ActionDispatch::Routing::RouteSet
+  #   def default_url_options(_options = {})
+  #     { locale: I18n.default_locale }
+  #   end
+  # end
 end
