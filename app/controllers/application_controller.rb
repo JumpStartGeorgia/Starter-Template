3612ab608875c243e4280ba6847a0ca0e8a9bd6c
nwshane <django09@gmail.com>
2015-10-19 12:44:43 +0400

    Small tweak in App controller for set locale function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_locale
-
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
+  before_action :set_locale
+  private :set_locale
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
