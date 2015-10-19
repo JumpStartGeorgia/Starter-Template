c95847c9f63342c843ea751bf412feb9fd6240ba
nwshane <django09@gmail.com>
2015-10-19 13:10:34 +0400

    Added locale files.

--- config/application.rb
+++ config/application.rb
@@ -25,8 +25,9 @@ module StarterTemplate
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # Load all locale files (including those nested in folders)
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
     config.i18n.default_locale = :en
 
     config.i18n.available_locales = [:en, :ka]
