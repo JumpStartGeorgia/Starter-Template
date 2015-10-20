ab727db7813239c813a7e2a5f22590431ee8497a
nwshane <django09@gmail.com>
2015-10-20 13:15:47 +0400

    Generated devise user files and config.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# Contains account data
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   scope ':locale', locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
+
     root 'root#index'
     get '/about' => 'root#about'
 
