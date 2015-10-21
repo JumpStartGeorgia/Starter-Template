4d1fc1535a2f2707a36903533782fe179c307849
nwshane <django09@gmail.com>
2015-10-21 11:33:49 +0400

    Added validations, belongs to role and is requested_role function to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ActiveRecord::Base
          :rememberable,
          :trackable,
          :validatable
+
+  belongs_to :role
+  validates :email, :role, presence: true
+
+  def is?(requested_role)
+    if role
+      role.name == requested_role
+    else
+      return false
+    end
+  end
 end
