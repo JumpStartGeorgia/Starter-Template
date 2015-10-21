20afd7d95c20a7dc236a9da0bf0cdfea609a3bc8
nwshane <django09@gmail.com>
2015-10-21 11:30:53 +0400

    Created and ran migration that adds roles.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_151_020_090_825) do
+ActiveRecord::Schema.define(version: 20_151_021_072_418) do
+  create_table 'roles', force: :cascade do |t|
+    t.string 'name',       limit: 255
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'email',                  limit: 255, default: '', null: false
     t.string 'encrypted_password',     limit: 255, default: '', null: false
