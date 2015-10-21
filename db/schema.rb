3b5de684e02e9fe42d24987d4a7bd2a05c1fd93e
nwshane <django09@gmail.com>
2015-10-21 11:38:10 +0400

    Added role id to user table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_151_021_072_418) do
+ActiveRecord::Schema.define(version: 20_151_021_073_628) do
   create_table 'roles', force: :cascade do |t|
     t.string 'name',       limit: 255
     t.datetime 'created_at'
@@ -31,8 +31,10 @@ ActiveRecord::Schema.define(version: 20_151_021_072_418) do
     t.string 'last_sign_in_ip',        limit: 255
     t.datetime 'created_at'
     t.datetime 'updated_at'
+    t.integer 'role_id',                limit: 4
   end
 
   add_index 'users', ['email'], name: 'index_users_on_email', unique: true, using: :btree
   add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
+  add_index 'users', ['role_id'], name: 'index_users_on_role_id', using: :btree
 end
