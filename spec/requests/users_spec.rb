a0a0bcab47cd92b2b3e063feea379d3a764558b6
nwshane <django09@gmail.com>
2015-10-21 12:49:06 +0400

    Added more request user specs.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,15 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  let(:site_admin_role) { FactoryGirl.create(:role, name: 'site_admin') }
+  let(:site_admin_user) { FactoryGirl.create(:user, role: site_admin_role) }
+
   before(:example) do
-    @role = FactoryGirl.create(:role, name: 'site_admin')
-    @user = FactoryGirl.create(:user, role: @role)
-    login_as(@user, scope: :user)
+    login_as(site_admin_user, scope: :user)
   end
 
   describe 'GET /users' do
     it 'works' do
       get users_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/1' do
+    it 'works' do
+      get user_path(site_admin_user)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/new' do
+    it 'works' do
+      get new_user_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/1/edit' do
+    it 'works' do
+      get edit_user_path(site_admin_user)
       expect(response).to have_http_status(200)
     end
   end
