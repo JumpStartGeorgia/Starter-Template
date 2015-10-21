a0cf86fb1eeacbe0a2d2d8634171838dc624106a
nwshane <django09@gmail.com>
2015-10-21 13:55:23 +0400

    Modified ActionController TestCase Behavior so that controller specs automatically use locale.

--- spec/helpers/locale_helper.rb
+++ spec/helpers/locale_helper.rb
@@ -7,6 +7,18 @@ RSpec.configure do |_config|
     I18n.l(string, options)
   end
 
+  # Makes controllers work with default locale
+  # Taken from http://stackoverflow.com/a/19079076/3115911
+  class ActionController::TestCase
+    module Behavior
+      def process_with_default_locale(action, http_method = 'GET', parameters = nil, session = nil, flash = nil)
+        parameters = { locale: I18n.locale }.merge(parameters || {}) unless I18n.locale.nil?
+        process_without_default_locale(action, http_method, parameters, session, flash)
+      end
+      alias_method_chain :process, :default_locale
+    end
+  end
+
   # Makes routing specs work with default locale
   class ActionDispatch::Routing::RouteSet
     def default_url_options(_options = {})
@@ -16,17 +28,7 @@ RSpec.configure do |_config|
 
   #### TODO COMMENTED OUT BELOW CODE BECAUSE IT'S COPIED FROM PRISONERS PROJECT AND NOT SURE IF IT'S NEEDED
 
-  # # Makes controllers work with default locale
-  # # Taken from http://stackoverflow.com/a/19079076/3115911
-  # class ActionController::TestCase
-  #   module Behavior
-  #     def process_with_default_locale(action, http_method = 'GET', parameters = nil, session = nil, flash = nil)
-  #       parameters = { locale: I18n.locale }.merge(parameters || {}) unless I18n.locale.nil?
-  #       process_without_default_locale(action, http_method, parameters, session, flash)
-  #     end
-  #     alias_method_chain :process, :default_locale
-  #   end
-  # end
+
   #
   # config.before(:each, type: :feature) do
   #   default_url_options[:locale] = I18n.default_locale
