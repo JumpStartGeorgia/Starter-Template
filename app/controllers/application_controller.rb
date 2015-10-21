912a35f90b4c684916eddc3d6fc83487c33032d7
nwshane <django09@gmail.com>
2015-10-21 11:05:38 +0400

    Added authorization methods to app controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  ##############################################
+  # Locales #
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
@@ -11,5 +14,28 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
+  end
+
+  ##############################################
+  # Authorization #
+
+  rescue_from CanCan::AccessDenied do |_exception|
+    if user_signed_in?
+      not_authorized
+    else
+      not_found
+    end
+  end
+
+  def not_authorized
+    redirect_to :back, alert: t('shared.msgs.not_authorized')
+  rescue ActionController::RedirectBackError
+    redirect_to root_path
+  end
+
+  def not_found(redirect_path = root_path)
+    Rails.logger.debug('Not found redirect')
+    redirect_to redirect_path,
+                notice: t('shared.msgs.does_not_exist')
   end
 end
