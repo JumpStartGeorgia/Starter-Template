3968a2a70fb8fd0cac325dd55539df306be2f96b
nwshane <django09@gmail.com>
2015-10-21 10:56:35 +0400

    Added locales to users routing spec. Closes #65

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: users_path).to route_to('users#index')
+      expect(get: users_path).to route_to('users#index', locale: 'en')
     end
 
     it 'routes to #new' do
-      expect(get: new_user_path).to route_to('users#new')
+      expect(get: new_user_path).to route_to('users#new', locale: 'en')
     end
 
     it 'routes to #show' do
-      expect(get: user_path(1)).to route_to('users#show', id: '1')
+      expect(get: user_path(:en, 1)).to route_to('users#show', id: '1', locale: 'en')
     end
 
     it 'routes to #edit' do
-      expect(get: edit_user_path(1)).to route_to('users#edit', id: '1')
+      expect(get: edit_user_path(:en, 1)).to route_to('users#edit', id: '1', locale: 'en')
     end
 
     it 'routes to #create' do
-      expect(post: users_path).to route_to('users#create')
+      expect(post: users_path).to route_to('users#create', locale: 'en')
     end
 
     it 'routes to #update' do
-      expect(put: user_path(1)).to route_to('users#update', id: '1')
+      expect(put: user_path(:en, 1)).to route_to('users#update', id: '1', locale: 'en')
     end
 
     it 'routes to #destroy' do
-      expect(delete: user_path(1)).to route_to('users#destroy', id: '1')
+      expect(delete: user_path(:en, 1)).to route_to('users#destroy', id: '1', locale: 'en')
     end
   end
 end
