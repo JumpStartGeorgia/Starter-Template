6a763b6548d57139062beb6958f3ba1ffdb67b8f
nwshane <django09@gmail.com>
2015-10-23 13:33:03 +0400

    Added mailer config to development env so that emails are caught by mailcatcher.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,11 @@ Rails.application.configure do
     Bullet.console = true
     Bullet.rails_logger = true
   end
+
+  # Setup email to be intercepted by mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    port: 1025
+  }
 end
