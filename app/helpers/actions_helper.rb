331c2e7eeb501daf2d95f96c848ff64b43d4ced0
nwshane <django09@gmail.com>
2015-10-23 11:37:25 +0400

    Replaced edit actions partial with helpers.

--- app/helpers/actions_helper.rb
+++ app/helpers/actions_helper.rb
@@ -1,11 +1,17 @@
 # Action button helpers, i.e. to easily create a 'Show User' button
 module ActionsHelper
-  def show_button
+  def view_button(object)
+    link_to t('shared.actions.with_obj.view_page',
+              obj: t("activerecord.models.#{object.model_name.singular}",
+                     count: 1)),
+            object,
+            class: 'btn btn-default'
   end
 
   def view_all_button(object)
     link_to t('shared.actions.with_obj.view_all',
-              obj: t("activerecord.models.#{object.model_name.singular}", count: 999)),
+              obj: t("activerecord.models.#{object.model_name.singular}",
+                     count: 999)),
             url_for(object),
             class: 'btn btn-default'
   end
