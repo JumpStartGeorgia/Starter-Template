280729d87187f4c5f4c955125e9c2bc791ee2d96
nwshane <django09@gmail.com>
2015-10-29 12:34:55 +0400

    Only sending exception errors on production and staging.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,7 +1,9 @@
-Rails.application.config.middleware
-  .use ExceptionNotification::Rack,
-       email: {
-         email_prefix: "[Bootstrap Starter App Error (#{Rails.env})] ",
-         sender_address: [ENV['APPLICATION_ERROR_FROM_EMAIL']],
-         exception_recipients: [ENV['APPLICATION_FEEDBACK_TO_EMAIL']]
-       }
+if Rails.env.production? || Rails.env.staging?
+  Rails.application.config.middleware
+    .use ExceptionNotification::Rack,
+         email: {
+           email_prefix: "[Bootstrap Starter App Error (#{Rails.env})] ",
+           sender_address: [ENV['APPLICATION_ERROR_FROM_EMAIL']],
+           exception_recipients: [ENV['APPLICATION_FEEDBACK_TO_EMAIL']]
+         }
+end
