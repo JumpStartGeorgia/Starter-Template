d63e7e391d5adae2046a9e0b68f140cb184bf339
nwshane <django09@gmail.com>
2015-10-29 12:32:49 +0400

    Code style fix.

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,6 +1,7 @@
-Rails.application.config.middleware.use ExceptionNotification::Rack,
-  email: {
-    email_prefix: "[Bootstrap Starter App Error (#{Rails.env})] ",
-    sender_address: [ENV['APPLICATION_ERROR_FROM_EMAIL']],
-    exception_recipients: [ENV['APPLICATION_FEEDBACK_TO_EMAIL']]
-  }
+Rails.application.config.middleware
+  .use ExceptionNotification::Rack,
+       email: {
+         email_prefix: "[Bootstrap Starter App Error (#{Rails.env})] ",
+         sender_address: [ENV['APPLICATION_ERROR_FROM_EMAIL']],
+         exception_recipients: [ENV['APPLICATION_FEEDBACK_TO_EMAIL']]
+       }
