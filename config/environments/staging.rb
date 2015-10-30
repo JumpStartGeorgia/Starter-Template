f6ba0ad7c618be70ce143aa01cd8904bf3f4597f
nwshane <django09@gmail.com>
2015-10-30 10:43:24 +0400

    Added staging default url host.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -82,5 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Recommended by Devise
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = {
+    host: 'dev-rails4-deploy.jumpstart.ge'
+  }
 end
