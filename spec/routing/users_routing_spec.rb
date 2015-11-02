28b943e757b42b5354444a0bef0265b89df06930
nwshane <django09@gmail.com>
2015-11-02 12:59:07 +0400

    Fixed routing specs broken by new namespace.

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -3,35 +3,38 @@ require 'rails_helper'
 RSpec.describe Admin::UsersController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: admin_users_path).to route_to('users#index', locale: 'en')
+      expect(get: admin_users_path)
+        .to route_to('admin/users#index', locale: 'en')
     end
 
     it 'routes to #new' do
-      expect(get: admin_new_user_path).to route_to('users#new', locale: 'en')
+      expect(get: new_admin_user_path)
+        .to route_to('admin/users#new', locale: 'en')
     end
 
     it 'routes to #show' do
       expect(get: admin_user_path(:en, 1))
-        .to route_to('users#show', id: '1', locale: 'en')
+        .to route_to('admin/users#show', id: '1', locale: 'en')
     end
 
     it 'routes to #edit' do
-      expect(get: admin_edit_user_path(:en, 1))
-        .to route_to('users#edit', id: '1', locale: 'en')
+      expect(get: edit_admin_user_path(:en, 1))
+        .to route_to('admin/users#edit', id: '1', locale: 'en')
     end
 
     it 'routes to #create' do
-      expect(post: admin_users_path).to route_to('users#create', locale: 'en')
+      expect(post: admin_users_path)
+        .to route_to('admin/users#create', locale: 'en')
     end
 
     it 'routes to #update' do
       expect(put: admin_user_path(:en, 1))
-        .to route_to('users#update', id: '1', locale: 'en')
+        .to route_to('admin/users#update', id: '1', locale: 'en')
     end
 
     it 'routes to #destroy' do
       expect(delete: admin_user_path(:en, 1))
-        .to route_to('users#destroy', id: '1', locale: 'en')
+        .to route_to('admin/users#destroy', id: '1', locale: 'en')
     end
   end
 end
