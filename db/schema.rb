e1a9fc12c147aaf9bd57958d7cd42b594be6c3f0
nwshane <django09@gmail.com>
2016-08-29 12:45:37 +0400

    Code style fix.

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_151_021_073_628) do
-  create_table 'roles', force: :cascade do |t|
-    t.string 'name',       limit: 255
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+ActiveRecord::Schema.define(version: 20160420110830) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "page_content_translations", force: :cascade do |t|
+    t.integer  "page_content_id", null: false
+    t.string   "locale",          null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "title"
+    t.text     "content"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email',                  limit: 255, default: '', null: false
-    t.string 'encrypted_password',     limit: 255, default: '', null: false
-    t.string 'reset_password_token',   limit: 255
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.integer 'sign_in_count',          limit: 4,   default: 0,  null: false
-    t.datetime 'current_sign_in_at'
-    t.datetime 'last_sign_in_at'
-    t.string 'current_sign_in_ip',     limit: 255
-    t.string 'last_sign_in_ip',        limit: 255
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer 'role_id',                limit: 4
+  add_index "page_content_translations", ["locale"], name: "index_page_content_translations_on_locale", using: :btree
+  add_index "page_content_translations", ["page_content_id"], name: "index_page_content_translations_on_page_content_id", using: :btree
+
+  create_table "page_contents", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', unique: true, using: :btree
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true, using: :btree
-  add_index 'users', ['role_id'], name: 'index_users_on_role_id', using: :btree
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "role_id"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["role_id"], name: "index_users_on_role_id", using: :btree
+
 end
