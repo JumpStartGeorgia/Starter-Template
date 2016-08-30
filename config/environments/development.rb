d61b57134090bb9abcdfe1e747c1270a4743f2a2
nwshane <django09@gmail.com>
2016-08-30 11:43:43 +0400

    Configured assets not to add logger output.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,9 @@ Rails.application.configure do
   # public folder. Necessary for custom mina assets deploy setup.
   config.assets.prefix = '/assets'
 
+  # Remove asset-related logger output
+  config.assets.quiet = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
