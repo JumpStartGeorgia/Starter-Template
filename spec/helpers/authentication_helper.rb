e23078d58bce31fedb71b6698b15a7a7cc25e44f
nwshane <django09@gmail.com>
2016-09-06 17:58:02 +0400

    Only including devise test controllerhelpers for controller specs.

--- spec/helpers/authentication_helper.rb
+++ spec/helpers/authentication_helper.rb
@@ -1,7 +1,7 @@
 require 'devise'
 
 RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
 
   # Warden is used to stub authentication in feature specs: https://github.com/plataformatec/devise/wiki/How-To:-Test-with-Capybara
